ca3453c1f4872142fbc09286849f9391694a7be5
mitanshu <mitanshugadariya.shivinfotech@gmail.com>
2025-08-14 18:53:22 +0530

    click button effect working

---
+++ ECommerce/Data/Model/RequestModelDemo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Data.Model
+{
+    public class RequestModelDemo
+    {
+        [Required(ErrorMessage = "Please Enter the Password")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter the Password")]
+        public string password { get; set; }
+    }
+}
